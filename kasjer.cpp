cb159a27bd108eb7a7e7ee1ccc7d6a5868574b93
koby1a <kobylarczykmateusz8@gmail.com>
2026-01-08 13:27:55 +0100

    v14

--- kasjer.cpp
+++ kasjer.cpp
@@ -5,44 +5,39 @@
 #include "serwis_ipc.h"
 
 int main() {
-    std::cout << "[kasjer] start procesu kasjera" << std::endl;
+    std::cout << "[kasjer] start" << std::endl;
 
     if (serwis_ipc_init() != SERWIS_IPC_OK) {
-        std::cerr << "[kasjer] blad inicjalizacji IPC" << std::endl;
+        std::cerr << "[kasjer] blad ipc_init" << std::endl;
         return EXIT_FAILURE;
     }
 
-    const int LICZBA_RAPORTOW_DO_OBSLUGI = 5;
+    const int MAKS_RAPORTOW = 500;
 
-    for (int i = 0; i < LICZBA_RAPORTOW_DO_OBSLUGI; ++i) {
+    for (int i = 0; i < MAKS_RAPORTOW; ++i) {
         int id_klienta = 0;
         int rzeczywisty_czas = 0;
         int koszt_koncowy = 0;
+        int stanowisko_id = 0;
         Samochod s{};
 
-        std::cout << "[kasjer] czekam na raport ("
-                  << (i + 1) << "/" << LICZBA_RAPORTOW_DO_OBSLUGI << ")"
-                  << std::endl;
+        std::cout << "[kasjer] czekam na raport (iteracja " << (i + 1) << ")" << std::endl;
 
-        if (serwis_ipc_odbierz_raport(id_klienta, rzeczywisty_czas, koszt_koncowy, s)
-            != SERWIS_IPC_OK) {
-            std::cerr << "[kasjer] blad odbierania raportu" << std::endl;
+        if (serwis_ipc_odbierz_raport(id_klienta, rzeczywisty_czas, koszt_koncowy, stanowisko_id, s) != SERWIS_IPC_OK) {
+            std::cerr << "[kasjer] blad odbioru raportu" << std::endl;
             break;
-            }
+        }
 
-        std::cout << "[kasjer] otrzymano raport: "
-                  << "id_klienta = " << id_klienta
-                  << ", marka = " << s.marka
-                  << ", rzeczywisty_czas = " << rzeczywisty_czas
-                  << ", koszt_koncowy = " << koszt_koncowy
+        std::cout << "[kasjer] raport: id_klienta=" << id_klienta
+                  << ", stanowisko=" << stanowisko_id
+                  << ", marka=" << s.marka
+                  << ", czas=" << rzeczywisty_czas
+                  << ", koszt=" << koszt_koncowy
+                  << " -> platnosc OK, wydanie kluczykow"
                   << std::endl;
-
-        // TODO:
-        //  - tutaj w przyszlosci mozna aktualizowac statystyki, licznik przyjetych itp.
     }
 
     serwis_ipc_cleanup();
-
-    std::cout << "[kasjer] koniec procesu kasjera" << std::endl;
+    std::cout << "[kasjer] koniec" << std::endl;
     return EXIT_SUCCESS;
 }
